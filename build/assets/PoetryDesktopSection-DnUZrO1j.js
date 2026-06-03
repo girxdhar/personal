75d6a0e0f0385d3cbd8649781f05f93706dd0352
giridhar <cpgiridhar@gmail.com>
2026-06-04 01:39:35 +0530

    UI changes

--- build/assets/PoetryDesktopSection-DnUZrO1j.js
+++ build/assets/PoetryDesktopSection-DnUZrO1j.js
@@ -1,4 +1,4 @@
-import{c as _,r as i,j as e,m as We,R as Le}from"./index-D6M9TeOD.js";/**
+import{c as _,r as i,j as e,m as We,R as Le}from"./index-CJovpPI3.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
